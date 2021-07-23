99e700583e72ee354b791fe7d92abbbc71264f18
skyang <uhmhaha@nate.com>
2021-07-23 17:31:07 +0900

    my

--- src/main/java/com/garak/ingestor/vo/EventVO.java
+++ src/main/java/com/garak/ingestor/vo/EventVO.java
@@ -6,21 +6,12 @@ import lombok.Setter;
 @Getter @Setter
 public class EventVO extends AbstractEvent {
 
-	private String eventId;
-	private String eventName;
-	private String eventCode;
-	private double eventLowLimit;
-	private double eventHighLimit;
-
-	
 	public EventVO () {
-		eventId
+		this.eventId = 
 		eventName
 		eventCode
 		eventLowLimit
 		eventHighLimit
-		warning = false;
-		error = false;
 		
 	}
 	
